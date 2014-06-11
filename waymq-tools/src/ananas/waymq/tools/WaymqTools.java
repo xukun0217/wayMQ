6d7d87f2e1b7224ea1f04337eb4bf132c3e46989
xukun <xukun0217@gmail.com>
2014-06-11 16:10:07 +0800

    xxx

--- waymq-tools/src/ananas/waymq/tools/WaymqTools.java
+++ waymq-tools/src/ananas/waymq/tools/WaymqTools.java
@@ -5,19 +5,17 @@ import ananas.waymq.tools.gui.MainFrame;
 public class WaymqTools {
 
 	public static void main(String[] arg) {
-
-		// EventRawDataProc proc = new EventRawDataProc();
-		// javax.swing.SwingUtilities.invokeLater(proc);
-
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-
-			@Override
-			public void run() {
-
-				MainFrame.show_it();
-
-			}
-		});
-
+		boolean old_style = true;
+		if (old_style) {
+			EventRawDataProc proc = new EventRawDataProc();
+			javax.swing.SwingUtilities.invokeLater(proc);
+		} else {
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					MainFrame.show_it();
+				}
+			});
+		}
 	}
 }
